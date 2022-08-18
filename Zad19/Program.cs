05dedfabddffbbfe32058818ab38dbdc2edf69b2
Vlad <isudmed@mail.ru>
2022-08-18 10:52:42 +0400

    first commit

---
+++ Zad19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[5] ||  number[1]==number[4] || number[2]==number[3] ){
+    Console.WriteLine($"Введеное число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Введеное число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 6){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите правильное число");
